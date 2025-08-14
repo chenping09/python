fdb2d293aef527b36deec1fdf185470854482dba
chenping <cp199009@gmail.com>
2025-08-14 17:13:18 +0900

    新加

---
+++ udemy_13_元组的用法.py
@@ -0,0 +1,10 @@
+info_tuple = ("张三",18,1.75,"张三")
+
+# 取值跟索引
+
+print(info_tuple[0])
+print(info_tuple.index("张三"))
+# 统计计数
+print(info_tuple.count("张三"))
+# 统计元组中包含元素的个数
+print(len(info_tuple))
---
+++ udemy_14_元组遍历.py
@@ -0,0 +1,5 @@
+info_tuple = ("张三",18,1.75)
+
+# 使用迭代遍历元组
+for item in info_tuple:
+    print(item)
