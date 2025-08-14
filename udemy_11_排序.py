db347f39b7a830905dec12b798be742beba26353
chenping <cp199009@gmail.com>
2025-08-14 16:23:54 +0900

    新加

---
+++ udemy_11_排序.py
@@ -0,0 +1,17 @@
+name_list = ["zhangsan","lisi","wangwu","wangxiaoge"]
+num_list = [1,11,4,8,2,7]
+
+
+# 升序
+# name_list.sort()
+# num_list.sort()
+# 降序
+# name_list.sort(reverse=True)
+# num_list.sort(reverse=True)
+
+# 逆转,反转
+name_list.reverse()
+num_list.reverse()
+
+print(name_list)
+print(num_list)
