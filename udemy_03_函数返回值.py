4a97a588c3485205b994d176e89bebbff50bf0f1
chenping <cp199009@gmail.com>
2025-08-14 10:11:32 +0900

    新加

--- udemy_03_函数返回值.py
+++ udemy_03_函数返回值.py
@@ -2,6 +2,7 @@ def sum_2_num(num1, num2):
     """对2个数进行求和"""
     result = num1 + num2
     return result
+    #return 表示返回,下方的代码不会被执行
 #可以使用变量,来接收函数执行的返回结果
 sum_result = sum_2_num(1, 2)
 print("计算的结果: %d " % sum_result)
