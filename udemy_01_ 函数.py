b3f7ff11bd0ff4cdfc806cef5b24c072a5f40ab2
chenping <cp199009@gmail.com>
2025-08-14 09:55:20 +0900

    this is first

---
+++ udemy_01_ 函数.py
@@ -0,0 +1,12 @@
+
+
+def say_hello():
+    """打招呼"""
+    print("Hello World!", end="")
+    print("Hello World!")
+    print("")
+    print("Hello World!")
+    print("Hello World!")
+    print("Hello World!")
+
+say_hello() 
---
+++ udemy_03_函数返回值.py
@@ -0,0 +1,7 @@
+def sum_2_num(num1, num2):
+    """对2个数进行求和"""
+    result = num1 + num2
+    return result
+#可以使用变量,来接收函数执行的返回结果
+sum_result = sum_2_num(1, 2)
+print("计算的结果: %d " % sum_result)
